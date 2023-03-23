447749912e347d24493ddadc9fc0fdd1b0dc7164
DianaNeumann <dmitryknyazkinofficial@gmail.com>
2023-03-23 08:02:34 +0300

    feat: add react frontend

---
+++ OMS.UI/src/App.js
@@ -0,0 +1,239 @@
+import React, { useState } from "react";
+import Constants from "./utilities/Constants";
+import OrderCreateForm from "./components/OrderCreateForm";
+import OrderUpdateForm from "./components/OrderUpdateForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function App() {
+  const [orders, setOrders] = useState([]);
+  const [showingCreateNewOrderForm, setShowingCreateNewOrderForm] = useState(false);
+  const [orderCurrentlyBeingUpdated, setOrderCurrentlyBeingUpdated] = useState(null);
+  const [startDate, setStartDate] = useState(new Date(new Date().setMonth(new Date().getMonth() - 1)));
+  const [endDate, setEndDate] = useState(new Date());
+
+  function getOrders() {
+    const url = Constants.API_URL_GET_ALL_ORDERS;
+
+    fetch(url, {
+      method: "GET"
+    })
+      .then(response => response.json())
+      .then(responseFromServer => {
+        setOrders(responseFromServer);
+        console.log(responseFromServer);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function deleteOrder(orderId) {
+    const url = `${Constants.API_URL_DELETE_ORDER}${orderId}`;
+    fetch(url, {
+      method: "DELETE"
+    })
+      .then(response => response.json())
+      .then(responseFromServer => {
+        console.log(responseFromServer);
+        onOrderDeleted(orderId);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function sendDate() {
+    const url = Constants.API_URL_GET_ORDER_SORTED_BY_DATERANGE;
+    const body = JSON.stringify({
+      start: startDate,
+      end: endDate
+    });
+    console.log(body);
+    fetch(url, {
+      method: "POST",
+      headers: {
+          "Accept": 'application/json',
+          "Content-Type": "application/json-patch+json"
+      },
+      body: body
+    })
+    .then(response => response.json())
+    .then(responseFromServer => {
+        console.log(responseFromServer);
+        setOrders(responseFromServer);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+
+  function sortByNumber() {
+    const url = Constants.API_URL_GET_ORDER_SORTED_BY_NUMBER;
+    fetch(url, {
+      method: "GET",
+      headers: {
+          "Accept": 'text/plain',
+      },
+    })
+    .then(response => response.json())
+    .then(responseFromServer => {
+        console.log(responseFromServer);
+        setOrders(responseFromServer);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+
+  function sortByProviderId() {
+    const url = Constants.API_URL_GET_ORDER_SORTED_BY_PROVIDERID;
+    fetch(url, {
+      method: "GET",
+      headers: {
+          "Accept": 'text/plain',
+      },
+    })
+    .then(response => response.json())
+    .then(responseFromServer => {
+        console.log(responseFromServer);
+        setOrders(responseFromServer);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          {(showingCreateNewOrderForm === false && orderCurrentlyBeingUpdated === null) && (
+            <div>
+              <h1>Order Managment System for Solforb</h1>
+
+              <div className="mt-5">
+                <button onClick={getOrders} className="btn btn-dark btn-lg w-100">Get Latest Orders from Server</button>
+                <button onClick={() => setShowingCreateNewOrderForm(true)} className="btn btn-secondary btn-lg w-100 mt-4">Create new Ordert</button>
+              </div>
+            </div>
+          )}
+
+          {(orders.length > 0 && showingCreateNewOrderForm === false && orderCurrentlyBeingUpdated === null) && renderOrdersTable()}
+
+          {showingCreateNewOrderForm && <OrderCreateForm onOrderCreated={onOrderCreated} />}
+
+          {orderCurrentlyBeingUpdated !== null && <OrderUpdateForm order={orderCurrentlyBeingUpdated} onOrderUpdated={onOrderUpdated} />}
+        </div>
+      </div>
+    </div>
+  );
+
+  function renderOrdersTable() {
+    return (
+      <>
+        <div>
+        <br></br>
+
+        <input className="datepicker" type ="date"   onChange={(e) => setStartDate(e.target.valueAsDate)}/>
+        <br></br>
+        
+        <input className="datepicker" type="date" onChange={(e) => setEndDate(e.target.valueAsDate)}/>
+        <br></br>
+
+        <button type="button" onClick={(e) => {
+          sendDate();
+        }}>Filter by date</button>
+        <br></br>
+        
+        <button type="button" onClick={(e) => {
+          sortByNumber();
+        }}>Sort by number</button>
+        <br></br>
+
+        <button type="button" onClick={(e) => {
+          sortByProviderId();
+        }}>Sort by providerId</button>
+       </div>
+        
+        <div className="table-responsive mt-5">
+          <table className="table table-bordered border-dark">
+            <thead>
+              <tr>
+                <th scope="col">Number</th>
+                <th scope="col">ProviderId</th>
+                <th scope="col">Date</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order.Id}>
+                  <th scope="row">{order.Number}</th>
+                  <td>{order.ProviderId}</td>
+                  <td>{order.Date}</td>
+                  <td>
+                    <button onClick={() => setOrderCurrentlyBeingUpdated(order)} className="btn btn-dark btn-lg mx-3 my-3">Update</button>
+                    <button onClick={() => { if (window.confirm(`Are you sure you want to delete the order with Id  "${order.Id}"?`)) deleteOrder(order.Id) }} className="btn btn-secondary btn-lg">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+        </div>
+      </>
+    );
+  }
+
+  function onOrderCreated(createdPost) {
+    setShowingCreateNewOrderForm(false);
+
+    if (createdPost === null) {
+      return;
+    }
+
+
+    getOrders();
+  }
+
+  function onOrderUpdated(updatedPost) {
+    setOrderCurrentlyBeingUpdated(null);
+
+    if (updatedPost === null) {
+      return;
+    }
+
+    let postsCopy = [...orders];
+
+    const index = postsCopy.findIndex((postsCopyPost, currentIndex) => {
+      if (postsCopyPost.Id === updatedPost.Id) {
+        return true;
+      }
+    });
+
+    if (index !== -1) {
+      postsCopy[index] = updatedPost;
+    }
+
+    setOrders(orders);
+
+  }
+
+  function onOrderDeleted(deletedOrderId) {
+    let ordersCopy = [...orders];
+
+    const index = ordersCopy.findIndex((copyOrder, currentIndex) => {
+      if (copyOrder.Id === deletedOrderId) {
+        return true;
+      }
+    });
+
+    if (index !== -1) {
+      ordersCopy.splice(index, 1);
+    }
+
+    setOrders(ordersCopy);
+
+  }
+}
---
+++ OMS.UI/src/components/OrderCreateForm.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react"
+import Constants from "../utilities/Constants"
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {DropdownContext} from "react-bootstrap/esm/DropdownContext";
+
+
+export default function OrderCreateForm(props) {    
+    const initialFormData = Object.freeze({
+        number: "X",
+        providerId: 2
+    });
+    const [formData, setFormData] = useState(initialFormData);
+    const [number, setNumber] = useState("");
+    const [date, setDate] = useState(new Date());
+    const [providerId, setProviderId] = useState("");
+    const [providers, setProviders] = useState([]);
+
+    useEffect(() => {
+        tempProviders();
+    }, []);
+    
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+
+
+    function getProviders(){
+        const url = Constants.API_URL_GET_ALL_PROVIDERS;
+    
+        fetch(url, {
+          method: "GET"
+        })
+          .then(response => response.json())
+          .then(providersFromServer => {
+            var tempProviders = providersFromServer.map(item => {
+                return { label: item.Name, value: item.Id };
+              });
+            setProviders(tempProviders);
+            console.log(providersFromServer);
+          })
+          .catch((error) => {
+            console.log(error);
+            alert(error);
+          });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const orderToCreate = {
+            number: number,
+            date: date,
+            providerId: providerId
+        };
+
+        const url = Constants.API_URL_CREATE_ORDER;
+        
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Accept": 'application/json',
+                "Content-Type": "application/json-patch+json"
+            },
+            body: JSON.stringify(orderToCreate)
+        })
+            .then(response => response.json())
+            .then(responseFromServer => {
+                console.log(responseFromServer);
+                console.log(JSON.stringify(orderToCreate))
+            })
+            .catch((error) => {
+                console.log(error);
+                console.log(orderToCreate)
+            });
+
+        props.onOrderCreated(orderToCreate);
+    };
+
+        
+    const url = Constants.API_URL_GET_ALL_PROVIDERS;
+    function tempProviders() {
+        fetch(url, {
+            method: "GET"
+          })
+            .then(response => response.json())
+            .then(providersFromServer => {
+              setProviders(providersFromServer);
+              console.log(providersFromServer);
+            })
+            .catch((error) => {
+              console.log(error);
+              alert(error);
+            });
+    }
+        
+    return (
+        <form className="w-100 px-5">
+            <h1 className="mt-5">Create new Order</h1>
+
+            <div className="mt-5">
+                <label className="h3 form-label">Order Number</label>
+                <input defaultValue={number} name="title" type="text" className="form-control" onChange={(event) => {
+                  setNumber(event.target.value);}} />
+            </div>
+
+            <div className="mt-4">
+                <label className="h3 form-label">Provider</label>
+                  <Select 
+                    onChange={(e) => {
+                        setProviderId(e.value);
+                    }} 
+                    options={providers.map((el) => (
+                    { 
+                        label: el.Name,
+                        value: el.Id
+                    }))} />
+            </div>
+
+            <button onClick={handleSubmit} className="btn btn-dark btn-lg w-100 mt-5">Submit</button>
+            <button onClick={() => props.onOrderCreated(null)} className="btn btn-secondary btn-lg w-100 mt-3">Cancel</button>
+        </form>
+    );
+}
